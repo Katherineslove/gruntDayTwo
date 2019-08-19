6595cb158dcee41e86843e72a3c3f69034234a8e
Katherineslove <48259743+Katherineslove@users.noreply.github.com>
2019-08-20 09:51:27 +1200

    jshint

--- Gruntfile.js
+++ Gruntfile.js
@@ -2,10 +2,22 @@ module.exports = function (grunt) {
 
   grunt.initConfig({
 
+    jshint: {
+        files: ['*.js', 'js/script.js'],
+        options: {
+            globals:{
+                jQuery: true
+            }
+        }
+    }
+
     ////// Katherine above Brayden below
 
   });
 
   ////// load tasks above register tasks below
+
+  grunt.loadNpmTasks('grunt-contrib-jshint');
+  grunt.registerTask('checkJS', ['jshint']);
 
 };
