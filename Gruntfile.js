5633ba02519ff51bfc4ff774a6e281e9893427f5
unknown <brayden.gray98@gmail.com>
2019-08-20 10:42:19 +1200

    css min after lint

--- Gruntfile.js
+++ Gruntfile.js
@@ -5,14 +5,29 @@ module.exports = function (grunt) {
     ////// Katherine above Brayden below
     csslint: {
       strict: {
-    options: {
-      import: 2
+        options: {
+          quiet: true,
+          import: 2
+        },
+        src: ['css/style.css']
+      },
     },
-    src: ['css/style.css']
-  },
+    cssmin: {
+      target: {
+        files: [{
+          expand: true,
+          cwd: 'css/',
+          src: ['*.css', '!*.min.css'],
+          dest: 'css/',
+          ext: '.min.css'
+        }]
+      }
     }
   });
   grunt.loadNpmTasks('grunt-contrib-csslint');
+  grunt.loadNpmTasks('grunt-contrib-cssmin');
   ////// load tasks above register tasks below
-  grunt.registerTask('checkCSS', ['csslint'])
+  grunt.registerTask('checkCSS', ['csslint']);
+  grunt.registerTask('runCSS', ['csslint','cssmin']);
+
 };
