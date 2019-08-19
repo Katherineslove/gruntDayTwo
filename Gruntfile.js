cf833646682cc2d1b08f94f71c7ca56edfae01c2
unknown <brayden.gray98@gmail.com>
2019-08-20 09:48:53 +1200

    Css lint added

--- Gruntfile.js
+++ Gruntfile.js
@@ -3,9 +3,16 @@ module.exports = function (grunt) {
   grunt.initConfig({
 
     ////// Katherine above Brayden below
-
+    csslint: {
+      strict: {
+    options: {
+      import: 2
+    },
+    src: ['css/style.css']
+  },
+    }
   });
-
+  grunt.loadNpmTasks('grunt-contrib-csslint');
   ////// load tasks above register tasks below
-
+  grunt.registerTask('checkCSS', ['csslint'])
 };
