e368eecc13e22300e333f964e25c1e2ca3e6dcb2
unknown <brayden.gray98@gmail.com>
2019-08-20 21:26:23 +1200

    api linked need own api key

--- js/script.js
+++ js/script.js
@@ -1,3 +1,39 @@
-let yeet = 'yeet';
-console.log("fetus deletus");
-console.log(yeet);
+$(document).ready(function(){
+
+
+$.ajax({
+  url: '../config.json',
+  type: 'GET',
+  dataType: 'json',
+  success: function(keys){
+    newsApiKey = keys['newsApiKey'];
+    getNewsData();
+  },
+  error: function(){
+    console.log("cannot find config.json file, cannot run application");
+  }
+});
+getNewsData = () => {
+  $.ajax({
+    url: `https://newsapi.org/v2/everything?apiKey=${newsApiKey}`,
+    type: 'GET',
+    dataType: 'json',
+    success: function(dataFromNewsApi){
+      console.log(dataFromNewsApi);
+      newsArticleList.empty();
+      if (dataFromNewsApi.articles.length === 0) {
+        console.log(`There are no news articles`);
+      } else {
+        dataFromNewsApi.articles.map(function(articles){
+          console.log(`${articles.title}, ${articles.description}`);
+        })
+      }
+    },
+    error: function(){
+      console.log("something went wrong");
+    }
+  })
+}
+
+
+});
